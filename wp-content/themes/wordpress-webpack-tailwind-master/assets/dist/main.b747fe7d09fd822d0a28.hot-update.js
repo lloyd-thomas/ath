1b7e8fda63f0745fd6381b6c524ee73e1ea3bfd9
Lloyd <lloyd@domh.net>
2020-10-12 15:27:59 +0200

    wordpress

---
+++ wp-content/themes/wordpress-webpack-tailwind-master/assets/dist/main.b747fe7d09fd822d0a28.hot-update.js
@@ -0,0 +1,17 @@
+webpackHotUpdate("main",{
+
+/***/ "":
+false,
+
+/***/ "./src/main.css":
+/*!**********************!*\
+  !*** ./src/main.css ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/main.css?");
+
+/***/ })
+
+})
